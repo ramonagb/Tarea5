d2ecdeec59790dc92651303fdf29ddbcc912033a
ramonagb <89026820+ramonagb@users.noreply.github.com>
2021-09-25 15:27:48 -0700

    Update exercise.py

--- assignments/02ContarParesImpares/src/exercise.py
+++ assignments/02ContarParesImpares/src/exercise.py
@@ -1,6 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    CajaV=list(input())
+    while CajaV[0] != '*':
+        CajaV.insert(0, input())
+
+    Pares=0
+    Impares=0
+    for i in CajaV[1:]:
+        i=int(i)
+        if i%2 == 0:
+            Pares += 1
+        else:
+            Impares += 1
+
+    print('PARES=' + str(Pares))
+    print('IMPARES=' + str(Impares))
 
 if __name__=='__main__':
     main()
